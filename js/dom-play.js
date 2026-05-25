ba6f38e46680cec1d353db59e07fdc34e8feaf35
Takeo Shimazu <takeo2039@gmail.com>
2026-05-24 17:14:02 -0700

    first commit

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,2 +1,41 @@
-alert("Hello from dom-play.js!");
-      
+/*
+ *
+ *    Create an app that on click actors lines are highlighted
+ *
+ *    With a single click, highlight (bold or otherwise) all lines for a specific actor in a play
+ *    Un-highlight (normal or otherwise) all lines from all other actors in the play
+ *            
+ *    The function(s) created to do the work must accommodate any number of actors, and any number of additional lines added to the page
+ *                 
+ *    HTML "hooks" may be added to the page for proper operation
+ *
+ *
+ *    */
+
+const spans = document.querySelectorAll("#play span");
+
+console.log(spans);
+
+
+const ophelia = document.querySelector(".button ophelia");
+const loertes = document.querySelector(".button loertes");
+const polonius = document.querySelector(".button polonius");
+
+ophelia.addEventListener
+function tell-your-name(
+/*
+for(const span of spans){
+
+  const actor = span.dataset.actor;
+
+  if (actor == "laertes") {
+    span.style.backgroundColor = "yellow";
+  }
+  else if (actor == "ophelia") {
+    span.style.backgroundColor = "pink";
+  }
+  else if (actor == "polonius") {
+    span.style.backgroundColor = "cyan";
+  }
+}
+*/
